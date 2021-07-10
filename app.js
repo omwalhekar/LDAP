691c882052e5d6e5b37bf634115c1e10fb19da04
omwalhekar <omwalhekar159@gmail.com>
2021-07-10 20:17:59 +0530

    adding a user

--- app.js
+++ app.js
@@ -2,14 +2,27 @@ const express = require("express");
 const app = express();
 const ldap = require("ldapjs");
 
+const client = ldap.createClient({
+  url: "ldap://127.0.0.1:10389",
+});
+
 app.listen(3000, () => {
   console.log("Server has started on port: 3000");
 });
 
 function authenticate(username, password) {
-  const client = ldap.createClient({
-    url: "ldap://127.0.0.1:10389",
+  client.bind(username, password, err => {
+    if (err) {
+      console.log("Error in bind: ", err);
+    } else {
+      console.log("success");
+      // searchUser();
+      addUser();
+    }
   });
+}
+
+function searchUser() {
   const opts = {
     //put an AND condition as below
     // filter: "(&(cn=Omkar)(sn=Walhekar))",
@@ -20,33 +33,41 @@ function authenticate(username, password) {
     attributes: ["sn", "cn"],
   };
 
-  client.bind(username, password, err => {
+  client.search("ou=users,ou=system", opts, (err, res) => {
     if (err) {
-      console.log("Error in bind: ", err);
+      console.log("Error in search: ", err);
     } else {
-      client.search("ou=users,ou=system", opts, (err, res) => {
-        if (err) {
-          console.log("Error in search: ", err);
-        } else {
-          res.on("searchRequest", searchRequest => {
-            console.log("searchRequest: ", searchRequest.messageID);
-          });
-          res.on("searchEntry", entry => {
-            console.log("entry: " + JSON.stringify(entry.object));
-          });
-          res.on("searchReference", referral => {
-            console.log("referral: " + referral.uris.join());
-          });
-          res.on("error", err => {
-            console.error("error: " + err.message);
-          });
-          res.on("end", result => {
-            console.log("status: " + result.status);
-          });
-        }
+      res.on("searchRequest", searchRequest => {
+        console.log("searchRequest: ", searchRequest.messageID);
+      });
+      res.on("searchEntry", entry => {
+        console.log("entry: " + JSON.stringify(entry.object));
+      });
+      res.on("searchReference", referral => {
+        console.log("referral: " + referral.uris.join());
+      });
+      res.on("error", err => {
+        console.error("error: " + err.message);
+      });
+      res.on("end", result => {
+        console.log("status: " + result.status);
       });
     }
   });
 }
-
+function addUser() {
+  const entry = {
+    // cn: "foo",
+    sn: "bar",
+    // email: ["foo@bar.com", "foo1@bar.com"],
+    objectclass: "inetOrgPerson",
+  };
+  client.add("cn=foo1, ou=users,ou=system", entry, err => {
+    if (err) {
+      console.log("err in new user" + err);
+    } else {
+      console.log("user added");
+    }
+  });
+}
 authenticate("cn=Omkar,ou=users,ou=system", "1");
