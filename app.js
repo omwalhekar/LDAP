cfd5c3982ef733c89b484cbe8930e56fb5f2ac5a
omwalhekar <omwalhekar159@gmail.com>
2021-07-10 17:10:58 +0530

    getting all users in an ou

--- app.js
+++ app.js
@@ -10,12 +10,37 @@ function authenticate(username, password) {
   const client = ldap.createClient({
     url: "ldap://127.0.0.1:10389",
   });
+  const opts = {
+    filter: "(objectClass=*)",
+    scope: "sub",
+    attributes: ["sn", "cn"],
+  };
 
   client.bind(username, password, err => {
     if (err) {
-      console.log(err.lde_message);
+      console.log("Error in bind: ", err);
     } else {
-      console.log("success");
+      client.search("ou=configuration,ou=system", opts, (err, res) => {
+        if (err) {
+          console.log("Error in search: ", err);
+        } else {
+          res.on("searchRequest", searchRequest => {
+            console.log("searchRequest: ", searchRequest.messageID);
+          });
+          res.on("searchEntry", entry => {
+            console.log("entry: " + JSON.stringify(entry.object));
+          });
+          res.on("searchReference", referral => {
+            console.log("referral: " + referral.uris.join());
+          });
+          res.on("error", err => {
+            console.error("error: " + err.message);
+          });
+          res.on("end", result => {
+            console.log("status: " + result.status);
+          });
+        }
+      });
     }
   });
 }
